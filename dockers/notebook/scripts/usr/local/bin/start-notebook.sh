5cb2733980c8fa25f63215c2b5f25c56020a2065
aristide <mendoo.aristide@gmail.com>
2024-11-15 20:09:05 +0000

    update lab extension

--- dockers/notebook/scripts/usr/local/bin/start-notebook.sh
+++ dockers/notebook/scripts/usr/local/bin/start-notebook.sh
@@ -8,6 +8,31 @@ set -e
 # JupyterLab by default
 DOCKER_STACKS_JUPYTER_CMD="${DOCKER_STACKS_JUPYTER_CMD:=lab}"
 
+# Check if the environment variable is set
+if [ -n "$PYTHON_PACKAGES" ]; then
+  echo "Installing additional Python packages..."
+  echo "$PYTHON_PACKAGES" | xargs -n 1 pip install
+else
+  echo "No additional Python packages to install."
+fi
+
+# Check if the environment variable is set
+if [ -n "$R_PACKAGES" ]; then
+  echo "Installing additional R packages..."
+  echo "$R_PACKAGES" | xargs -n 1 install2.r --error
+else
+  echo "No additional R packages to install."
+fi
+
+# Check if RUN_SCRIPT is set and not empty
+if [ ! -z "$RUN_SCRIPT" ]; then
+  echo "Running script from RUN_SCRIPT environment variable:"
+  echo "$RUN_SCRIPT" > /tmp/script.sh
+  chmod +x /tmp/script.sh
+  bash /tmp/script.sh
+  rm /tmp/script.sh
+fi
+
 if [[ -n "${JUPYTERHUB_API_TOKEN}" ]]; then
     echo "WARNING: using start-singleuser.sh instead of start-notebook.sh to start a server associated with JupyterHub."
     exec /usr/local/bin/start-singleuser.sh "$@"
--- dockers/notebook/scripts/usr/local/bin/start-singleuser.sh
+++ dockers/notebook/scripts/usr/local/bin/start-singleuser.sh
@@ -4,6 +4,31 @@
 
 set -e
 
+# Check if the environment variable is set
+if [ -n "$PYTHON_PACKAGES" ]; then
+  echo "Installing additional Python packages..."
+  echo "$PYTHON_PACKAGES" | xargs -n 1 pip install
+else
+  echo "No additional Python packages to install."
+fi
+
+# Check if the environment variable is set
+if [ -n "$R_PACKAGES" ]; then
+  echo "Installing additional R packages..."
+  echo "$R_PACKAGES" | xargs -n 1 install2.r --error
+else
+  echo "No additional R packages to install."
+fi
+
+# Check if RUN_SCRIPT is set and not empty
+if [ ! -z "$RUN_SCRIPT" ]; then
+  echo "Running script from RUN_SCRIPT environment variable:"
+  echo "$RUN_SCRIPT" > /tmp/script.sh
+  chmod +x /tmp/script.sh
+  bash /tmp/script.sh
+  rm /tmp/script.sh
+fi
+
 # set default ip to 0.0.0.0
 if [[ "${NOTEBOOK_ARGS} $*" != *"--ip="* ]]; then
     NOTEBOOK_ARGS="--ip=0.0.0.0 ${NOTEBOOK_ARGS}"
